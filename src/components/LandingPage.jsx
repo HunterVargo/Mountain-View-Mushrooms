fecbf8d6ac189526c5930e392988da7435402c5c
Hunter Vargo <hunters7629@gmail.com>
2023-06-12 00:51:04 -0400

    added logo, worked on locaton and market widgets

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import './LandingPage.css';
 import { Link } from "react-router-dom";
+import Widget from './MainWidget.jsx';
 export default function LandingPage(){
 
     return(
         <div id="LandingContainer">
-            <div id="svg"></div>
+            
             <div id="header">
-                <Link className="LandPageLinks" to="/Product">Product</Link>
-                <Link className="LandPageLinks" to="/SporeFlow">Software</Link>
-                <Link className="LandPageLinks" to="/Media">Media</Link>
-            </div>
-            <div id="bodyLandingPage">
-                <div id="TextContentLandingPage">
-                    
-                    <h1 id="MVMh1">Mountain View <br/>Mushrooms</h1>
-                    <p id="MVMp">Supplier of specialty mushrooms<br/> located in Northeast, PA. </p>
-                    <div id="buttonDiv">
-                        <button className="ButtonLanding">Mushrooms</button>
-                        <button className="ButtonLanding">Culinary</button>
-                    </div>
+                <div id="svg"></div>
+                <div id="linkBox">
+                    <Link className="LandPageLinks" to="/Product">Product</Link>
+                    <Link className="LandPageLinks" to="/SporeFlow">Software</Link>
+                    <Link className="LandPageLinks" to="/Media">Media</Link>
                 </div>
+            </div>
+            <div id="allContentMain">
+                <div id="scedualBox">
+                   <Widget />
+                   <Widget />
+                   <Widget />
+                   <Widget />
+                   <Widget />
                 
+                </div>
+
+                <div id="bodyLandingPage">    
+                    <div id="TextContentLandingPage">
+                        <h1 id="MVMh1">Mountain View <br/>Mushrooms</h1>
+                        <p id="MVMp">Supplier of specialty mushrooms<br/> located in Northeast, PA. </p>
+                        <div id="buttonDiv">
+                            <button className="ButtonLanding">Mushrooms</button>
+                            <button className="ButtonLanding">Culinary</button>
+                        </div>
+                    </div>
+                
+                </div>
             </div>
         </div>
         
---
+++ src/components/MainWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MainWidgetStyle.css"
+
+export default function Widget(){
+    
+
+    return(
+        <div id="widgetBox">
+            <h2>Farmers Market</h2>
+            <p>location</p>
+        </div>
+    )
+}
