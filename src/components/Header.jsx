36344632d724cbe935201f7b3777292835a3f9d5
Hunter Vargo <hunters7629@gmail.com>
2023-06-26 14:58:57 -0400

    png fix on header plus filter shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./LandingPage.css"
 import { Link } from "react-router-dom";
-
+// import logo from "./public/logo.png"
 export default function Header(){
 
     return(
         <div id="header">
-            <div id="svg"></div>
+            <img id="logo"src="/logo.png" alt="Logo" />
             <div id="linkBox">
                 <Link className="LandPageLinks" to="/Product">Product</Link>
                 <Link className="LandPageLinks" to="/SporeFlow">Software</Link>
