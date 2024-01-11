b21548d5bf13409af8e7ee6040ad354b445701dd
Hunter Vargo <hunters7629@gmail.com>
2024-01-10 20:14:47 -0500

    Add: Mobile Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,35 +3,40 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex bg-EerieBlack text-GhostWhite justify-between items-end px-20 py-8 ">
-      <div className="font-lilitaOne text-HunyadiYellow text-4xl">MVM</div>
-      <nav className="space-x-16 font-inter">
-        <NavLink
-          className={({ isActive }) => (isActive ? "font-bold" : null)}
-          to={"/"}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "font-bold" : null)}
-          to={"/product"}
-        >
-          Products
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "font-bold" : null)}
-          to={"/software"}
-        >
-          Software
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "font-bold" : null)}
-          style={{ color: "#ECA72C" }}
-          to={"/about"}
-        >
-          About Me
-        </NavLink>
-      </nav>
+    <div className="sm:flex bg-EerieBlack text-GhostWhite justify-between items-end sm:px-10 py-8 ">
+      <div className="text-center font-lilitaOne text-HunyadiYellow text-4xl">
+        MVM
+      </div>
+
+      <div className="flex  justify-center w-full sm:w-fit mt-6 sm:ml-6 px-6">
+        <nav className="sm:space-x-10 font-inter w-full flex justify-between">
+          <NavLink
+            className={({ isActive }) => (isActive ? "font-bold" : null)}
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "font-bold" : null)}
+            to={"/product"}
+          >
+            Products
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "font-bold" : null)}
+            to={"/software"}
+          >
+            Software
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "font-bold" : null)}
+            style={{ color: "#ECA72C" }}
+            to={"/about"}
+          >
+            About Me
+          </NavLink>
+        </nav>
+      </div>
     </div>
   );
 };
