59baeb126ad22a81a55f8aa2ae870bd45a29d040
Hunter Vargo <hunters7629@gmail.com>
2024-01-18 14:15:08 -0500

    Add: new header functionality when on recipe page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const Header = () => {
   const { id } = useParams();
-  console.log(!id);
-
+  const location = useLocation().pathname.split("/");
+  const last = location[location.length - 1];
+  console.log(location);
   return (
     <>
       {!id ? (
         <div className="sm:flex bg-EerieBlack text-GhostWhite justify-between items-end sm:px-10 py-8 ">
-          <div className="text-center font-lilitaOne text-HunyadiYellow text-4xl">
-            MVM
-          </div>
+          {last === "recipes" ? (
+            <div className="text-center font-lilitaOne text-HunyadiYellow text-4xl">
+              Recipes
+            </div>
+          ) : (
+            <div className="text-center font-lilitaOne text-HunyadiYellow text-4xl">
+              MVM
+            </div>
+          )}
 
           <div className="flex  justify-center w-full sm:w-fit mt-6 sm:ml-6 px-6">
             <nav className="sm:space-x-10 font-inter w-full flex justify-between">
