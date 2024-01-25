5284843b96a030cb0468389f01260dd46fc5e49b
Hunter Vargo <hunters7629@gmail.com>
2024-01-24 21:47:49 -0500

    Add: style and print remove functionality

--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -6,20 +6,35 @@ const Recipes = () => {
   const Recipes = ProductPgData.data.Recipes;
 
   return (
-    <div className="p-6 w-full">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {Recipes.map((recipe) => {
-          return (
-            <Link
-              className="bg-HunyadiYellow text-bold aspect-square flex justify-center items-center"
-              to={recipe.id}
-              key={recipe.id}
-            >
-              <img src="" alt="" />
-              <h2 className="text-center">{recipe.Recipe}</h2>
-            </Link>
-          );
-        })}
+    <div className="p-6 w-full ">
+      <div className="flex justify-center">
+        <div>
+          <h1 className="p-6 text-center text-5xl font-semibold ">
+            Select Recipe
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-6 sm:w-maxView tab:p-20">
+            {Recipes.map((recipe) => {
+              return (
+                <div className=" hover:rounded-3xl overflow-hidden hover:tab:font-bold rounded hover:border-Jasmine hover:border-6 shadow-lg">
+                  <Link
+                    className="text-bold flex justify-center items-center"
+                    to={recipe.id}
+                    key={recipe.id}
+                  >
+                    <img
+                      className=""
+                      src={`/Img/${recipe.Image}`}
+                      alt="RecipeImage"
+                    />
+                    <h2 className="absolute text-3xl font-extrabold font-lilitaOne tracking-wider text-EerieBlack p-6 drop-shadow-lg">
+                      {recipe.Recipe}
+                    </h2>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Pages/SelectedRecipe.jsx
+++ src/Pages/SelectedRecipe.jsx
@@ -1,38 +1,84 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProductPgData from "../Data/ProductPgData";
 import { useParams } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { AiOutlineRollback } from "react-icons/ai";
+import { IoPrint } from "react-icons/io5";
 const SelectedRecipe = () => {
   const { id } = useParams();
   const Recipes = ProductPgData.data.Recipes;
   const SelectedRecipe = id === Recipes[id - 1].id ? Recipes[id - 1] : null;
+  const [printStyle, setPrintStyle] = useState(true);
+  const isPrintable = useRef(false);
+
+  function togglePrintStyles() {
+    setPrintStyle(false);
+    isPrintable.current = true;
+    console.log(isPrintable);
+  }
+  useEffect(() => {
+    if (isPrintable.current) {
+      isPrintable.current = false;
+      window.print();
+      console.log("working");
+      setPrintStyle(true);
+    }
+  }, [printStyle]);
 
   return (
-    <div className="p-6">
-      <div className="bg-Jasmine p-2">
-        <div key={SelectedRecipe.id}>
-          <h1 className="text-center font-semibold">{SelectedRecipe.Recipe}</h1>
-          <img className="aspect-video " src="" alt="RecipeImage" />
-          <p>{SelectedRecipe.Description}</p>
-          <h2>Ingrediants</h2>
-          <ol className="list-disc list-inside">
-            {SelectedRecipe.Ingredients.map((ingredient, index) => {
-              return <li key={index}>{ingredient}</li>;
-            })}
-          </ol>
-          <h2>Preparation</h2>
-          <ol className="list-disc list-inside">
-            {SelectedRecipe.Perparation.map((prep, index) => {
-              return <li key={index}>{prep}</li>;
-            })}
-          </ol>
-          <p>
-            <span className="font-semibold">Note:</span>
-            {SelectedRecipe.Note}
-          </p>
+    <>
+      <div className="p-6">
+        <div className="bg-Jasmine p-6 tab:p-10">
+          <div key={SelectedRecipe.id}>
+            <div
+              className={`flex justify-evenly tab:justify-start ${
+                printStyle ? null : "hidden"
+              }`}
+            >
+              <div className="bg-HunyadiYellow tab:w-10 h-10 w-1/4 rounded-3xl flex justify-center items-center hover:text-3xl hover:tab:w-12 hover:tab:h-12 tab:fixed">
+                <Link
+                  className="w-full flex justify-center"
+                  to=".."
+                  relative="path"
+                >
+                  <AiOutlineRollback />
+                </Link>
+              </div>
+              <div className="bg-HunyadiYellow tab:w-10 h-10 w-1/4 rounded-3xl flex justify-center items-center hover:text-3xl hover:tab:w-12 hover:tab:h-12 tab:fixed tab:ml-14 relative ">
+                <button
+                  onClick={() => togglePrintStyles()}
+                  className="w-full flex justify-center"
+                >
+                  <IoPrint />
+                </button>
+              </div>
+            </div>
+
+            <h1 className="text-center font-semibold text-5xl my-6">
+              {SelectedRecipe.Recipe}
+            </h1>
+
+            <p className="text-2xl">{SelectedRecipe.Description}</p>
+            <h2 className="text-2xl font-semibold my-6">Ingrediants</h2>
+            <ol className="list-disc list-inside text-2xl">
+              {SelectedRecipe.Ingredients.map((ingredient, index) => {
+                return <li key={index}>{ingredient}</li>;
+              })}
+            </ol>
+            <h2 className="text-2xl font-semibold my-6">Preparation</h2>
+            <ol className="list-disc list-inside text-2xl">
+              {SelectedRecipe.Perparation.map((prep, index) => {
+                return <li key={index}>{prep}</li>;
+              })}
+            </ol>
+            <p className="text-2xl my-6">
+              <span className="font-semibold text-2xl">Note:</span>
+              {SelectedRecipe.Note}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
